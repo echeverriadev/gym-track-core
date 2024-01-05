e442cbbf6a296a17c28d79b2f9fd70283686059c
Carlos Echeverría <55212109+echeverriadev@users.noreply.github.com>
2024-01-04 21:57:13 -0500

    feat(user): extend service to use abstract crud service

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -15,29 +15,30 @@ import { CreateUserRequestDto } from '../dtos/request/create-user.request.dto';
 import { UserResponseDto } from '../dtos/respose/user.response.dto';
 import { UpdateUserRequestDto } from '../dtos/request/update-user.request.dto';
 import { ErrorResponseDto } from 'src/dtos/error.response.dto';
-
+import * as bcrypt from 'bcrypt';
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
 @UsePipes(new ValidationPipe())
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Post()
-  create(
+  async create(
     @Body() createUserDto: CreateUserRequestDto,
   ): Promise<UserResponseDto | ErrorResponseDto> {
+    createUserDto.password = await bcrypt.hash(createUserDto.password, 10);
     return this.usersService.create(createUserDto);
   }
 
   @Get()
   findAll(): Promise<UserResponseDto[] | ErrorResponseDto> {
-    return this.usersService.findAll();
+    return this.usersService.find({});
   }
 
   @Get(':id')
   findOne(
     @Param('id') id: string,
   ): Promise<UserResponseDto | ErrorResponseDto> {
-    return this.usersService.findOne(id);
+    return this.usersService.findById(id);
   }
 
   @Patch(':id')
--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,99 +1,26 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../entities/user.entity';
-import { CreateUserRequestDto } from '../dtos/request/create-user.request.dto';
 import { UserResponseDto } from '../dtos/respose/user.response.dto';
-import { UpdateUserRequestDto } from '../dtos/request/update-user.request.dto';
-import * as bcrypt from 'bcrypt';
-import { ErrorResponseDto } from 'src/dtos/error.response.dto';
+import { AbstractCrudService } from 'src/services/abstract-crud.service';
 
 @Injectable()
-export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
-
-  async create(
-    user: CreateUserRequestDto,
-  ): Promise<UserResponseDto | ErrorResponseDto> {
-    const hashedPassword = await bcrypt.hash(user.password, 10);
-    user.password = hashedPassword;
-
-    return this.userModel
-      .create(user)
-      .then((user) => {
-        return this.#responseWithClientOrException(user);
-      })
-      .catch((err) => ({
-        statusCode: 500,
-        message: 'Error creating user',
-        error: err,
-      }));
+export class UsersService extends AbstractCrudService<User> {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {
+    super(userModel);
   }
 
-  findAll(): Promise<UserResponseDto[] | ErrorResponseDto> {
-    return this.userModel
-      .find()
-      .then((users) => {
-        if (!users) {
-          return;
-        }
-        return users.map((user) => this.#mapEntityToDto(user));
-      })
-      .catch((err) => ({
-        statusCode: 500,
-        message: 'Error finding users',
-        error: err,
-      }));
-  }
-
-  findOne(id: string): Promise<UserResponseDto | ErrorResponseDto> {
-    return this.userModel
-      .findById(id)
-      .then((user) => {
-        return this.#responseWithClientOrException(user);
-      })
-      .catch((err) => ({
-        statusCode: 500,
-        message: 'Error finding user',
-        error: err,
-      }));
-  }
-
-  update(
-    id: string,
-    updateUser: UpdateUserRequestDto,
-  ): Promise<UserResponseDto | ErrorResponseDto> {
-    return this.userModel
-      .findByIdAndUpdate(id, updateUser, { new: true })
-      .then((user) => {
-        return this.#responseWithClientOrException(user);
-      })
-      .catch((err) => ({
-        statusCode: 500,
-        message: 'Error updating user',
-        error: err,
-      }));
-  }
-
-  #responseWithClientOrException(user: void | User): UserResponseDto {
-    if (!user) {
-      throw new NotFoundException(`User not found`);
-    }
-    return this.#mapEntityToDto(user);
-  }
-
-  #mapEntityToDto(user): UserResponseDto {
-    const response = new UserResponseDto();
-    response._id = user._id.toHexString();
-    response.firstName = user.firstName;
-    response.lastName = user.lastName;
-    response.email = user.email;
-    response.birthDay = user.birthDay;
-    response.height = user.height;
-    response.gender = user.gender;
-    response.status = user.status;
-    response.createdAt = user.createdAt;
-    response.updatedAt = user.updatedAt;
-    return response;
+  protected mapEntityToDto(record: User): UserResponseDto {
+    const dto = new UserResponseDto();
+    dto._id = record.id;
+    dto.firstName = record.firstName;
+    dto.lastName = record.lastName;
+    dto.birthDay = record.birthDay;
+    dto.height = record.height;
+    dto.gender = record.gender;
+    dto.email = record.email;
+    dto.status = record.status;
+    return dto;
   }
 }
