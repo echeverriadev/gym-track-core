9541a87d57a73e10fda79db001ec09161b5ee88b
Carlos Echeverria <echeverriadev@gmail.com>
2024-02-14 20:54:38 -0500

    feat(main): enable cors and add enviroment vars

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  config();
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT;
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  app.setGlobalPrefix('api');
   await app.listen(port);
 }
 bootstrap();
